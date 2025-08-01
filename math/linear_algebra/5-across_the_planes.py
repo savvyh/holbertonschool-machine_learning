b1dff56cda17c9f6f3324cef915a7ca22f711a09
savvyh <7277@holbertonstudents.com>
2025-08-01 12:43:21 +0000

    feat: write a python script that adds to matrices

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module that adds two matrices
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two matrices
+    """
+    if len(mat1) != len(mat2):
+        return None
+
+    if len(mat1[0]) != len(mat2[0]):
+        return None
+
+    new_matrix = []
+    for i in range(len(mat1)):
+        for j in range(len(mat2)):
+            new_matrix.append(mat1[i][j] + mat2[i][j])
+
+    return new_matrix
