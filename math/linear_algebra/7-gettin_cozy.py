8372f27411743130612745ff5d4bb2d1d81eb544
savvyh <7277@holbertonstudents.com>
2025-08-01 13:52:55 +0000

    feat: write a python script that concatenates two matrices along a specific axis

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module that concatenates two matrices along a specific axis
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Concatenates two matrices along a specific axis
+    """
+    if len(mat1) != len(mat2):
+        return None
+
+    new_matrix = []
+    if axis == 0:
+        new_matrix = mat1 + mat2
+    elif axis == 1:
+        for i in range(len(mat1)):
+            new_matrix.append(mat1[i] + mat2[i])
+
+    return new_matrix
