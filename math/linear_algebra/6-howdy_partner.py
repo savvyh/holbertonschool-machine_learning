de04e72d5643b03ce362fc4ac4d8183ab2307691
savvyh <7277@holbertonstudents.com>
2025-08-01 12:54:57 +0000

    feat: write a python script that concatenates two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays
+    """
+    new_list = (arr1 + arr2)
+    return new_list
