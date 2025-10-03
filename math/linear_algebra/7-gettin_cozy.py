997055c30a2fda0fec7e82222c983686319fd6c2
savvyh <7277@holbertonstudents.com>
2025-10-03 10:19:06 +0000

    fix: correct axis logical

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,14 +8,16 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """
     Concatenates two matrices along a specific axis
     """
-    if len(mat1) != len(mat2):
-        return None
-
     new_matrix = []
     if axis == 0:
         new_matrix = mat1 + mat2
     elif axis == 1:
         for i in range(len(mat1)):
             new_matrix.append(mat1[i] + mat2[i])
+        return new_matrix
+    else:
+        new_matrix = mat1 + mat2
+        new_matrix.append(mat1[i] + mat2[i])
+        return new_matrix
 
     return new_matrix
